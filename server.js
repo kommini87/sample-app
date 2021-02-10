46a90784489f94ea59884018d4d834647bfff00f
kommini87 <40634719+kommini87@users.noreply.github.com>
2021-02-10 17:11:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:yellow;">Hello Upender welcome to AWS DevOps !!</h1> \n');
+  res.send('<h1 style="color:blue;">Hello Upender welcome to AWS DevOps !!</h1> \n');
 });
 
 app.listen(PORT, HOST);
